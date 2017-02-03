3a419c946169da8a4014854ebac688087a3077de
S Fujimoto <unicoshun@gmail.com>
2017-02-03 20:23:32 +0900

    2.8

--- PhotoGroove.elm
+++ PhotoGroove.elm
@@ -1,40 +1,45 @@
 module PhotoGroove exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing(..)
+import Html.Attributes exposing (..)
 
-urlPrefix =
-  "http://elm-in-action.com/"
-
-view model =
-  div [ class "content" ]
-      [ h1 [] [ text "Photo Groove"]
-      , div [ id "thumbnails" ]
-        (List.map (\photo -> viewThumbnail model.selectedUrl photo)
-          model.photos
-        )
-      , img
-        [ class "large"
-        , src (urlPrefix ++ "large/" ++ model.selectedUrl)
-        ]
-        []
-      ]
-
-viewThumbnail selectedUrl thumbnail =
-  img
-    [ src (urlPrefix ++ thumbnail.url)
-    , classList [("selected", selectedUrl == thumbnail.url)]
-    ]
-    []
 
 initialModel =
-  { photos =
-    [ { url = "1.jpeg"}
-    , { url = "2.jpeg"}
-    , { url = "3.jpeg"}
-    ]
-  , selectedUrl = "1.jpeg"
-  }
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+view model =
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , div [ id "thumbnails" ]
+            (List.map (\photo -> viewThumbnail model.selectedUrl photo)
+                model.photos
+            )
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
+        ]
+
+
+viewThumbnail selectedUrl thumbnail =
+    img
+        [ src (urlPrefix ++ thumbnail.url)
+        , classList [ ( "selected", selectedUrl == thumbnail.url ) ]
+        ]
+        []
+
 
 main =
-  view initialModel
+    view initialModel
